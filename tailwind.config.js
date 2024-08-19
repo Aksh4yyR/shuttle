b789bdded63d6f888b9e4f0c18913439cf246de8
Aksh4yyR <akshayraveendran559@gmail.com>
2024-08-20 01:23:49 +0530

    shuttle

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'fl-green':'#33CC33'
+      }
+    },
+  },
+  plugins: [],
+}
+
